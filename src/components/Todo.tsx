1ef420150bcf2b172aa2a40d9c3a8dd8ce82ce01
Henrique <henrique.dias@gruponewway.com.br>
2023-05-06 15:35:34 -0300

    chore: adding font-awesome

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,4 +1,6 @@
-import React, { ChangeEventHandler } from 'react';
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faThumbsUp, faThumbsDown } from '@fortawesome/free-solid-svg-icons'
 
 export interface TodoProps {
   id: string;
@@ -13,10 +15,13 @@ const Todo: React.FC<TodoProps> = ({ id, complete = false, content = '', onToggl
       <div className="wrapper-todo border border-gray-500 rounded p-2">
         <div className="body grid grid-cols-12">
           <div className="complete col-span-1">
-            <input type="checkbox" checked={complete} onChange={() => onToggleComplete && onToggleComplete(id)} />
+            <label className='cursor-pointer' >
+              <input className='hidden' type="checkbox" checked={complete} onChange={() => onToggleComplete && onToggleComplete(id)} />
+              <FontAwesomeIcon className={`${complete ? 'text-blue-500' : ''}`} icon={complete ? faThumbsUp : faThumbsDown} />
+            </label>
           </div>
           <div className="content col-span-11" title={id}>
-            <div>{content}</div>
+            <div className={`${complete ? 'line-through text-blue-500' : ''}`}>{content}</div>
           </div>
         </div>
       </div>
